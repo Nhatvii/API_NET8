f35a9a579a62f1bee12a362c8cef753b83a94e8e
Nhatvii <55909032+Nhatvii@users.noreply.github.com>
2026-02-04 14:07:02 +0700

    Update WeatherForecastController.cs

--- APICore_NET8/APICore_NET8/Controllers/WeatherForecastController.cs
+++ APICore_NET8/APICore_NET8/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace APICore_NET8.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-         "Gà", "Heo", "Bò", "vịt"
+         "Gà", "Heo", "Bò", "Vịt cạp cạp"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
